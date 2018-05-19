59ec9ab9ace52ba4006ed945a9901869f8d65538
thingsareright <2271366490@qq.com>
2018-05-19 12:08:17 +0800

    把天气引入了，墨迹天气API，阿里云产品市场引进。

---
+++ src/main/java/com/demo/util/AuthService.java
@@ -0,0 +1,127 @@
+package com.demo.util;
+
+import com.baidu.aip.face.AipFace;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 获取token类
+ */
+public class AuthService {
+
+    private static final String APP_ID = "11264831";
+    private static final String API_KEY = "LdPPlGBPDqGuDv8O0zr2dMgC";
+    private static final String SECRET_KEY = "8xhGjvXXzC2eG8ukqaAP4SF09pdGWBgw";
+    private static final AipFace client = new AipFace(APP_ID, API_KEY, SECRET_KEY);
+
+    /**
+     * 获取权限token
+     * @return 返回示例：
+     * {
+     * "access_token": "24.460da4889caad24cccdb1fea17221975.2592000.1491995545.282335-1234567",
+     * "expires_in": 2592000
+     * }
+     */
+    public static String getAuth() {
+        // 官网获取的 API Key 更新为你注册的
+        String clientId = "LdPPlGBPDqGuDv8O0zr2dMgC";
+        // 官网获取的 Secret Key 更新为你注册的
+        String clientSecret = "8xhGjvXXzC2eG8ukqaAP4SF09pdGWBgw";
+        return getAuth(clientId, clientSecret);
+    }
+
+    /**
+     * 获取API访问token
+     * 该token有一定的有效期，需要自行管理，当失效时需重新获取.
+     * @param ak - 百度云官网获取的 API Key
+     * @param sk - 百度云官网获取的 Securet Key
+     * @return assess_token 示例：
+     * "24.460da4889caad24cccdb1fea17221975.2592000.1491995545.282335-1234567"
+     */
+    public static String getAuth(String ak, String sk) {
+        // 获取token地址
+        String authHost = "https://aip.baidubce.com/oauth/2.0/token?";
+        String getAccessTokenUrl = authHost
+                // 1. grant_type为固定参数
+                + "grant_type=client_credentials"
+                // 2. 官网获取的 API Key
+                + "&client_id=" + ak
+                // 3. 官网获取的 Secret Key
+                + "&client_secret=" + sk;
+        try {
+            URL realUrl = new URL(getAccessTokenUrl);
+            // 打开和URL之间的连接
+            HttpURLConnection connection = (HttpURLConnection) realUrl.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.err.println(key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String result = "";
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+            /**
+             * 返回结果示例
+             */
+            System.err.println("result:" + result);
+            JSONObject jsonObject = new JSONObject(result);
+            String access_token = jsonObject.getString("access_token");
+            return access_token;
+        } catch (Exception e) {
+            System.err.printf("获取token失败！");
+            e.printStackTrace(System.err);
+        }
+        return null;
+    }
+
+    public static AipFace getAirFace() {
+
+        return client;
+    }
+
+    public static void detect(AipFace client, String image /*这里是BASE64字符串*/) {
+        // 传入可选参数调用接口
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("face_field", "age");
+        options.put("max_face_num", "2");
+        options.put("face_type", "LIVE");
+
+        if (null == image)
+            image = "dsalkd";
+        String imageType = "BASE64";
+
+        // 人脸检测
+        JSONObject res = client.detect(image, imageType, options);
+        System.out.println(res.toString(2));
+
+    }
+
+    
+
+    public static void main(String[] args) {
+        detect(client,"jdasd");
+    }
+
+
+
+
+
+
+
+
+}
